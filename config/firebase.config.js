9663222010eca0376da64bfac6bb923f10cb0b3f
asyrafduyshart <123456789believE>
2019-08-05 09:21:38 +0700

    delete firebase config

--- config/firebase.config.js
+++
@@ -1,8 +0,0 @@
-export default {
-  apiKey: 'AIzaSyB8MR5Ynk-XFCzAnC982tyM6VbI77cvf_8',
-  authDomain: 'lido-streaming.firebaseapp.com',
-  databaseURL: 'https://lido-streaming.firebaseio.com',
-  projectId: 'lido-streaming',
-  storageBucket: 'lido-streaming.appspot.com',
-  messagingSenderId: '443643882331',
-};
